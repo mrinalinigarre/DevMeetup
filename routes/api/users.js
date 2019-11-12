1a29013e65af14e64e38ccda2381c69cd57a1a3e
mrinalinigarre <mrinalinigml@gmail.com>
2019-11-12 14:48:30 -0800

    Created User Model

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
 //@access Public
-//@route GET
+//@route POST api/users
+//@desc register user
 
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 8 or more characters'
+    ).isLength({ min: 8 })
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('User route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //connecting to database
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
 
 //Define routes
