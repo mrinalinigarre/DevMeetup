a94bb8b54cb8403b8910a8250b6be976151a553f
mrinalinigarre <gmlmrinalini@gmail.com>
2019-11-15 23:57:15 -0800

    obtained current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 //@access Public
-//@route GET/profile
+//@desc get curr user profile
+//@route GET/profile/me
 
-router.get('/', (req, res) => res.send('Profile route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'No such user profile' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('server error');
+  }
+});
 
 module.exports = router;
