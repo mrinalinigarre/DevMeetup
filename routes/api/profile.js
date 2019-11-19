b058a3fde41bbab07650becc6a798f981a5a5a74
mrinalinigarre <gmlmrinalini@gmail.com>
2019-11-18 21:29:13 -0800

    Added Social links field

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -39,7 +39,7 @@ router.post(
       check('status', 'Status is required')
         .not()
         .isEmpty(),
-      check('skill', 'Skills is required')
+      check('skills', 'Skills is required')
         .not()
         .isEmpty()
     ]
