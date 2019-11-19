293ead5516491d3c59f2610df311b81207996898
mrinalinigarre <gmlmrinalini@gmail.com>
2019-11-18 22:10:39 -0800

    Get user profile by id and all user profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -98,8 +98,49 @@ router.post(
       res.status(500).send('Server Error');
     }
 
-    console.log(profileFields.skills);
+    //console.log(profileFields.skills);
   }
 );
+
+//@access public
+//@desc get all user profile
+//@route GET/profile
+
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+//@access public
+//@desc get all user profile
+//@route GET/profile/user/:user_id
+
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile)
+      return res
+        .status(400)
+        .json({ msg: 'No profile found with this user_id' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'OnjectId') {
+      return res
+        .status(400)
+        .json({ msg: 'No profile found with this user_id' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
