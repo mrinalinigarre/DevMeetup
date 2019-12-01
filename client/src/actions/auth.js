2ec28fd9e6eedcc1ff0b62eaaa75c46db9618a82
mrinalinigarre <gmlmrinalini@gmail.com>
2019-12-01 14:52:00 -0800

    Added user logout feature

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,8 @@ import {
   USER_LOADED,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGOUT
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -84,4 +85,9 @@ export const login = (email, password) => async dispatch => {
       type: LOGIN_FAIL
     });
   }
+};
+
+//Logout clear profile
+export const logout = () => dispatch => {
+  dispatch({ type: LOGOUT });
 };
